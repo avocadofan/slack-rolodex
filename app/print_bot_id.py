6ad26e9555c584722d77133ec76987bc091b0e73
Chris Day <prototype625@gmail.com>
2017-02-03 22:26:44 -0700

    added bot envs

---
+++ app/print_bot_id.py
@@ -0,0 +1,15 @@
+import os
+from slackclient import slackclient
+
+BOT_NAME = 'starterbot'
+
+if __name__ == '__main__':
+	api_call = slack_client.api_call("users.list")
+	if api_call.get('ok'):
+		users=api_call.get('members')
+		for user in users:
+			if 'name' in user and user.get('name') == BOT_NAME
+				print("Bot ID for '" + user['name'] + "' is "+ user.get('id'))
+			else:
+				print("could not find bot user with the name " + BOT_NAME)
+			
